02f14213acc05fb0f278cae689ee0e034e0e6583
Mattis Hallberg <skatemattis97@hotmail.com>
2024-01-12 14:34:00 +0100

    Deleted babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,18 @@
-const path = require('path');
+const path = require("path")
 
 module.exports = {
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-        use: {
-        loader: 'babel-loader',
-        options: {
-            presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ]
-          },
-        },
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
-  mode: 'development'
-};
+}
