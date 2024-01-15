617f26b045ecb5e7975d2f5c98b98b065a798e97
Mattis Hallberg <skatemattis97@hotmail.com>
2024-01-15 19:42:49 +0100

    First UI progress in expandProject file

--- src/expandProject.js
+++ src/expandProject.js
@@ -8,5 +8,30 @@ export default function expandProject(
   priority,
   todoListItems
 ) {
+  removeAllChildNodes(document.getElementById("right-content"))
+  updateTitleAndDate(title)
   console.log(title, description, dueDate, priority, todoListItems)
+
+  //
+  const Header = document.createElement("div")
+  Header.classList.add("Header")
+  Header.innerHTML = `${title} ${description} ${dueDate} ${priority}`
+  //
+  const todoListandNotes = document.createElement("div")
+  todoListandNotes.classList.add("todoListandNotes")
+  const todoList = document.createElement("div")
+  todoList.classList.add("todoList")
+
+  todoList.innerHTML = "todo list"
+
+  const Notes = document.createElement("div")
+  Notes.classList.add("Notes")
+  Notes.innerHTML = "notes"
+
+  todoListandNotes.appendChild(todoList)
+  todoListandNotes.appendChild(Notes)
+
+  //
+  document.getElementById("right-content").appendChild(Header)
+  document.getElementById("right-content").appendChild(todoListandNotes)
 }
