4906445d866ed4be702b54b6220baee90b77ab26
Mattis Hallberg <skatemattis97@hotmail.com>
2024-01-21 16:11:20 +0100

    Added UI to add todo list items and CSS linethrough when checkbox is checked

--- src/expandProject.js
+++ src/expandProject.js
@@ -39,25 +39,18 @@ export default function expandProject(
 
   const listItemsContainer1 = document.createElement("div")
   listItemsContainer1.classList.add("listItemsContainer1")
+  listItemsContainer1.id = "listItemsContainer1"
   todoList.appendChild(listItemsContainer1)
 
-  for (let i = 0; i < project.todoListItems.length; i++) {
-    console.log(project.todoListItems[i])
-    let listItemInput = document.createElement("input")
-    listItemInput.type = "checkbox"
-    listItemInput.name = project.todoListItems[i]
+  displayTodoList()
 
-    let listItemLabel = document.createElement("label")
-    listItemLabel.innerHTML = project.todoListItems[i]
-    listItemLabel.for = project.todoListItems[i]
-
-    let listItemDiv = document.createElement("div")
-    listItemDiv.classList.add("listItemDiv")
-    listItemDiv.appendChild(listItemInput)
-    listItemDiv.appendChild(listItemLabel)
-
-    listItemsContainer1.appendChild(listItemDiv)
-  }
+  todoListInputBtn.addEventListener("click", (e) => {
+    if (todoListInput.value == "") return
+    e.preventDefault()
+    project.todoListItems.push(todoListInput.value)
+    localStorage.setItem(title, JSON.stringify(project))
+    displayTodoList()
+  })
 
   // notes
   const Notes = document.createElement("div")
@@ -83,4 +76,28 @@ export default function expandProject(
   //
   document.getElementById("right-content").appendChild(Header)
   document.getElementById("right-content").appendChild(todoListandNotes)
+
+  //
+  function displayTodoList() {
+    if (listItemsContainer1.childNodes.length != 0) {
+      removeAllChildNodes(document.getElementById("listItemsContainer1"))
+    }
+    for (let i = 0; i < project.todoListItems.length; i++) {
+      let listItemInput = document.createElement("input")
+      listItemInput.classList.add("checkboxInput")
+      listItemInput.type = "checkbox"
+      listItemInput.name = project.todoListItems[i]
+
+      let listItemLabel = document.createElement("label")
+      listItemLabel.innerHTML = project.todoListItems[i]
+      listItemLabel.for = project.todoListItems[i]
+
+      let listItemDiv = document.createElement("div")
+      listItemDiv.classList.add("listItemDiv")
+      listItemDiv.appendChild(listItemInput)
+      listItemDiv.appendChild(listItemLabel)
+
+      listItemsContainer1.appendChild(listItemDiv)
+    }
+  }
 }
